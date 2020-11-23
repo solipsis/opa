12d2951cd31238adcb1c0e997cf94dea909de5f5
dave <davida3141@gmail.com>
2020-11-23 16:36:29 -0700

    update docs and constants to remove unimplemented NoSignature algorithm

--- internal/jwx/jwa/signature.go
+++ internal/jwx/jwa/signature.go
@@ -9,24 +9,23 @@ import (
 // SignatureAlgorithm represents the various signature algorithms as described in https://tools.ietf.org/html/rfc7518#section-3.1
 type SignatureAlgorithm string
 
-var signatureAlg = map[string]struct{}{"ES256": {}, "ES384": {}, "ES512": {}, "HS256": {}, "HS384": {}, "HS512": {}, "PS256": {}, "PS384": {}, "PS512": {}, "RS256": {}, "RS384": {}, "RS512": {}, "none": {}}
+var signatureAlg = map[string]struct{}{"ES256": {}, "ES384": {}, "ES512": {}, "HS256": {}, "HS384": {}, "HS512": {}, "PS256": {}, "PS384": {}, "PS512": {}, "RS256": {}, "RS384": {}, "RS512": {}}
 
 // Supported values for SignatureAlgorithm
 const (
-	ES256       SignatureAlgorithm = "ES256" // ECDSA using P-256 and SHA-256
-	ES384       SignatureAlgorithm = "ES384" // ECDSA using P-384 and SHA-384
-	ES512       SignatureAlgorithm = "ES512" // ECDSA using P-521 and SHA-512
-	HS256       SignatureAlgorithm = "HS256" // HMAC using SHA-256
-	HS384       SignatureAlgorithm = "HS384" // HMAC using SHA-384
-	HS512       SignatureAlgorithm = "HS512" // HMAC using SHA-512
-	NoSignature SignatureAlgorithm = "none"
-	PS256       SignatureAlgorithm = "PS256" // RSASSA-PSS using SHA256 and MGF1-SHA256
-	PS384       SignatureAlgorithm = "PS384" // RSASSA-PSS using SHA384 and MGF1-SHA384
-	PS512       SignatureAlgorithm = "PS512" // RSASSA-PSS using SHA512 and MGF1-SHA512
-	RS256       SignatureAlgorithm = "RS256" // RSASSA-PKCS-v1.5 using SHA-256
-	RS384       SignatureAlgorithm = "RS384" // RSASSA-PKCS-v1.5 using SHA-384
-	RS512       SignatureAlgorithm = "RS512" // RSASSA-PKCS-v1.5 using SHA-512
-	NoValue     SignatureAlgorithm = ""      // No value is different from none
+	ES256   SignatureAlgorithm = "ES256" // ECDSA using P-256 and SHA-256
+	ES384   SignatureAlgorithm = "ES384" // ECDSA using P-384 and SHA-384
+	ES512   SignatureAlgorithm = "ES512" // ECDSA using P-521 and SHA-512
+	HS256   SignatureAlgorithm = "HS256" // HMAC using SHA-256
+	HS384   SignatureAlgorithm = "HS384" // HMAC using SHA-384
+	HS512   SignatureAlgorithm = "HS512" // HMAC using SHA-512
+	PS256   SignatureAlgorithm = "PS256" // RSASSA-PSS using SHA256 and MGF1-SHA256
+	PS384   SignatureAlgorithm = "PS384" // RSASSA-PSS using SHA384 and MGF1-SHA384
+	PS512   SignatureAlgorithm = "PS512" // RSASSA-PSS using SHA512 and MGF1-SHA512
+	RS256   SignatureAlgorithm = "RS256" // RSASSA-PKCS-v1.5 using SHA-256
+	RS384   SignatureAlgorithm = "RS384" // RSASSA-PKCS-v1.5 using SHA-384
+	RS512   SignatureAlgorithm = "RS512" // RSASSA-PKCS-v1.5 using SHA-512
+	NoValue SignatureAlgorithm = ""      // No value is different from none
 )
 
 // Accept is used when conversion from values given by
